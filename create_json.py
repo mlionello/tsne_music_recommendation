e5a911c30c198b67d903cc77df982205c4caa807
Matteo Lionello <lionello.matteo@gmail.com>
2017-04-30 16:04:02 +0200

    new features added

--- create_json.py
+++ create_json.py
@@ -68,20 +68,36 @@ data = np.asarray(data)
 elms = []
 for i in range(n_samples):
 	elm = {}
+	elm['artist']=data[i,1]
+	elm['title']=data[i,2]
+	elm['x']=float(pred[i,0])
+	elm['y']=float(pred[i,1])
 	elm['joy'] = (float(vectors[i,15])) 
 	elm['angry'] = (float(vectors[i,12]))
 	elm['sad']=float(vectors[i,16])
 	elm['tender']=(float(vectors[i,17]))
 	elm['erotic']=(float(vectors[i,13]))
-	elm['y']=(float(pred[i,1]))
-	elm['x']=(float(pred[i,0]))
-	elm['fear']=(float(vectors[i,3]))
-	elm['artist']=data[i,1]
-	elm['title']=data[i,2]
-	elm['spotifyID']=(data[i,3])
-	elm['x']=float(pred[i,0])
-	elm['y']=float(pred[i,1])
+	elm['fear']=(float(vectors[i,14]))
+	elm['blues']=(float(vectors[i,0]))
+	elm['country']=(float(vectors[i,1]))
+	elm['easylistening']=(float(vectors[i,2]))
+	elm['electronica']=(float(vectors[i,3]))
+	elm['folk']=(float(vectors[i,4]))
+	elm['hiphopurban']=(float(vectors[i,5]))
+	elm['jazz']=(float(vectors[i,6]))
+	elm['latin']=(float(vectors[i,7]))
+	elm['newage']=(float(vectors[i,8]))
+	elm['pop']=(float(vectors[i,9]))
+	elm['rnbsoul']=(float(vectors[i,10]))
+	elm['rock']=(float(vectors[i,11]))
+	elm['gospel']=(float(vectors[i,18]))
+	elm['reggae']=(float(vectors[i,19]))
+	elm['world']=(float(vectors[i,20]))
+	#elm['spotifyID']=(data[i,3])
 	elms.append(elm)
+#0: 'Blues','Country','EasyListening','Electronica','Folk','HipHopUrban','Jazz' ,'Latin','NewAge','Pop','RnBSoul','Rock',
+#12: 'Angry','Erotic','Fear','Joy','Sad' ,'Tender','Gospel','Reggae','World',
+#21: 'BeatType','ChoirType','ClassicalMainGenre' ,'ProminentVoiceType','RhythmicEnsemble','ProminentInstrument','CompositionStyleEra' ,'SoundPeriod','BeatImpact','ClassicalCrossover','ClassicalEnsemble','SoundTexture','TempoFeel'
 data = np.asarray(data)
 output_str = json.dumps(elms, separators=(',',':'))
 output_file = open("data.json", "w")
